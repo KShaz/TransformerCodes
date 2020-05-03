460ae07ce3ee6faaf52e88a07c5de8333562e330
s2dream <s2dream@gmail.com>
2020-05-04 00:56:09 +0900

    Initial Codes

---
+++ gpu_test.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+# a = tf.test.is_gpu_available(
+#     cuda_only=False, min_cuda_compute_capability=None
+# )
+#
+# print(a)
+
+if tf.test.gpu_device_name():
+
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+
+else:
+
+   print("Please install GPU version of TF")
+
+
