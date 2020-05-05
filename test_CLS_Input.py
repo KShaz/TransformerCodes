6ae53723a8a59e12aa41fe0de35e5edf0bdf3445
s2dream <s2dream@gmail.com>
2020-05-05 15:46:29 +0900

    test_CLS_input

--- test_CLS_Input.py
+++ test_CLS_Input.py
@@ -7,17 +7,17 @@ arg_max = tf.constant([[1,2,3,4,5,0],
                        [13,14,15,16,17,18]
                       ])
 
+#
+# pos_hot = tf.constant([[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0],
+#                        [0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
+#                        [0,1,0,0,1,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0]])
 
-position_hot = tf.constant([[0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,1,1,0,0,0,0],
-                [0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
-                [0,1,0,0,1,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0]])
-
-position_list = tf.constant([[6,13,15,16, -1, -1],
-                             [3, 9,13, -1, -1, -1],
-                             [1, 4, 5,10,15,18]])
+pos_list = tf.constant([[6, 13, 15, 16, -1, -1],
+                        [3, 9,13, -1, -1, -1],
+                        [1, 4, 5,10,15,18]])
 
 
-c = tf.one_hot(position_list, 21) # [batch, max_len_pos, seq_len]
+c = tf.one_hot(pos_list, 21) # [batch, max_len_pos, seq_len]
 c = tf.cast(c, dtype=tf.int32)
 arg_max = tf.expand_dims(arg_max, 2) #  [batch, max_len_pos, 1]
 # arg_max = tf.tile(arg_max, [1,1, 21])
